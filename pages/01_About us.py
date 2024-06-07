974afb086ec9b1850b0fbd6ca27312f517cc1095
Godfred Mante <godfred.mante@gmail.com>
2024-06-07 12:42:43 +0200

    text update

--- pages/01_About us.py
+++ pages/01_About us.py
@@ -1,16 +1,29 @@
 import streamlit as st
 
-st.markdown('''Meet the Visionaries Behind TechMatch
+st.title('Ceci est un titre')
+st.header('Ceci est un en-tête')
+st.subheader('Ceci est un sous-en-tête')
+st.text('Ceci est un texte simple')
 
+st.title('''Meet the Visionaries Behind TechMatch''')
+
+st.markdown('''
 Safa, Godfred, and Nicolas – a trio of innovative thinkers and technology enthusiasts – are the masterminds behind TechMatch. With their diverse backgrounds and shared passion for simplifying technology, they have come together to create a groundbreaking tool that is transforming how we approach tech solutions.
+''')
 
-Safa
-A seasoned software engineer and visionary leader, Safa brings a wealth of technical expertise and strategic insight to TechMatch. With over a decade of experience in the tech industry, she has a deep understanding of software development and user experience. Safa's relentless pursuit of innovation ensures that TechMatch stays ahead of the curve, continually evolving to meet the needs of its users.
+st.markdown('''**Safa**''')
 
-Godfred
-Godfred is a renowned tech entrepreneur with a strong background in business development and project management. His ability to identify market needs and translate them into actionable solutions has been instrumental in shaping TechMatch. Godfred's entrepreneurial spirit and customer-centric approach drive the platform's growth, ensuring that it delivers maximum value to its users.
+st.markdown('''A seasoned software engineer and visionary leader, Safa brings a wealth of technical expertise and strategic insight to TechMatch. With over a decade of experience in the tech industry, she has a deep understanding of software development and user experience. Safa's relentless pursuit of innovation ensures that TechMatch stays ahead of the curve, continually evolving to meet the needs of its users.
+            ''')
 
-Nicolas
-An expert in data science and artificial intelligence, Nicolas is the technical genius behind TechMatch's advanced algorithms. His expertise in machine learning and data analytics powers the recommendation engine that lies at the heart of TechMatch. Nicolas's commitment to leveraging cutting-edge technology guarantees that users receive the most accurate and relevant tool recommendations.
 
-Together, Safa, Godfred, and Nicolas have combined their unique skills and experiences to create TechMatch, a tool that not only simplifies the search for the perfect tech solutions but also empowers users to achieve their goals with confidence.''')
+st.markdown('''**Godfred**''')
+st.markdown('''Godfred is a renowned tech entrepreneur with a strong background in business development and project management. His ability to identify market needs and translate them into actionable solutions has been instrumental in shaping TechMatch. Godfred's entrepreneurial spirit and customer-centric approach drive the platform's growth, ensuring that it delivers maximum value to its users.
+            ''')
+
+st.markdown('''**Nicolas**''')
+st.markdown('''An expert in data science and artificial intelligence, Nicolas is the technical genius behind TechMatch's advanced algorithms. His expertise in machine learning and data analytics powers the recommendation engine that lies at the heart of TechMatch. Nicolas's commitment to leveraging cutting-edge technology guarantees that users receive the most accurate and relevant tool recommendations
+            ''')
+
+st.markdown('''Together, Safa, Godfred, and Nicolas have combined their unique skills and experiences to create TechMatch, a tool that not only simplifies the search for the perfect tech solutions but also empowers users to achieve their goals with confidence.
+            ''')
